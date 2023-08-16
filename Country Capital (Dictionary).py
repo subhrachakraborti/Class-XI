6a28c492edb9f7ff32fabfc1f354ae32d634dd68
Subhra Chakraborti <cers.subhra@gmail.com>
2023-08-16 19:10:07 +0530

    Add files via upload

---
+++ Country Capital (Dictionary).py
@@ -0,0 +1,5 @@
+country = {"India":"New Delhi","Japan":"Tokyo","USA":"Washington D.C.","Russia":"Moscow","Ukraine":"Kiev"}
+l = len(country)
+print("Length = ",l)
+cntry = input("Enter Country Name: \n")
+print(country[cntry])
